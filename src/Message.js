8494dcb413246b51d1ab6948a94da4e0beaa4191
Maja Glowania <maiaglowania13@gmail.com>
2025-12-14 13:57:05 +0100

    changed message css

--- src/Message.js
+++ src/Message.js
@@ -2,17 +2,18 @@ import styled from "styled-components";
 
 export const Message = styled.p`
   text-align: center;
-  font-size: 1.2em;
+  font-size: 16px;
   font-weight: bold;
   padding: 20px;
   border-radius: 8px;
   max-width: 500px;
   margin: 50px auto;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  background-color: ${({ theme }) => theme.colors.backgroundLight};
-  border: 2px solid
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+  background-color: ${({ $isError }) => ($isError ? "white" : "transparent")};
+  border: 4px solid
     ${({ $isError, theme }) =>
       $isError ? theme.colors.errorRed : theme.colors.successGreen};
+
   color: ${({ $isError, theme }) =>
     $isError ? theme.colors.errorRed : theme.colors.fontDark};
 `;
